bc9aa470491b204582913b0481d4db807523a416
acid-burn117 <osoriodouglas19@gmail.com>
2022-03-04 17:08:06 -0500

    fixed rest

--- reset.js
+++ reset.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-if (fs.existsSync('./dist/team.html')) {
-  fs.unlinkSync('./dist/team.html');
+if (fs.existsSync('./dist/teammembers.html')) {
+  fs.unlinkSync('./dist/teammembers.html');
   console.log('/dist/ folder reset!');
 }
